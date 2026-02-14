cd491b88e6c6735c0d9b8eb26ba20bae0b83afad
Dorouthas <xX11ove@outlook.com>
2026-02-14 21:13:25 +0800

    增加kd 及 胜率 快捷获取方法

--- BuGLandAPI/BuGLandAPI/DTOs/PlayerGameStateData.cs
+++ BuGLandAPI/BuGLandAPI/DTOs/PlayerGameStateData.cs
@@ -43,4 +43,57 @@ public class PlayerGameStateData
 
         [JsonPropertyName("banned")]
         public bool Banned { get; set; } 
+        
+        //便捷方法: 获取玩家KD
+        public (string normalKd, string fkKd) GetTotalKd(GameType gameTypeKey)
+        {
+                var gameData = GetGameData(gameTypeKey);
+                if (gameData == null) return ("0.00", "0.00");
+                int totalKills = 0;
+                int totalDeaths = 0;
+                int totalFinalKills = 0;
+                int totalFinalDeaths = 0;
+                foreach (var kvp in gameData)
+                {
+                        var stats = kvp.Value;
+                        if (stats == null) continue;
+                        totalKills += stats.Kills;
+                        totalDeaths += stats.Deaths;
+                        totalFinalKills += stats.FinalKills;
+                        totalFinalDeaths += stats.FinalDeaths;
+                }
+
+                double normalKdValue = totalDeaths > 0 ? (double)totalKills / totalDeaths : totalKills;
+                double fkKdValue =
+                        totalFinalDeaths > 0
+                                ? (double)totalFinalKills / totalFinalDeaths
+                                : totalFinalKills; // 保留两位小数并返回字符串
+                return (normalKdValue.ToString("F2"), fkKdValue.ToString("F2")); 
+        }
+        
+        //便捷方法: 获取玩家对应游戏胜率
+        public string GetTotalWinRate(GameType gameTypeKey)
+        {
+                var gameData = GetGameData(gameTypeKey);
+                if (gameData == null) return "0.00%";
+                int totalWin = 0;
+                int totalGame = 0;
+                foreach (var kvp in gameData)
+                {
+                        var stats = kvp.Value;
+                        if (stats == null) continue;
+                        totalWin += stats.Win;
+                        totalGame += stats.Game;
+                }
+
+                double winRate = totalGame > 0 ? (double)totalWin / totalGame * 100 : 0;
+                return $"{winRate:F2}%";
+        }
+        
+        //便捷方法: 获取玩家总胜率
+        public double GetWinRate()
+        {
+                double winRate = TotalGame > 0 ? (double)TotalWin / TotalGame * 100 : 0;
+                return winRate;
+        }
 }
